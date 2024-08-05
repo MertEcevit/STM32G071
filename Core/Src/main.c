891e245a3fbbe236315d49c7c35efe4d7c2ce19c
Mert Ecevit <68374019+MertEcevit@users.noreply.github.com>
2024-08-05 21:51:07 +0300

    Update main.c

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -1,36 +1,40 @@
-// STM32G071RB mikrodenetleyicisinin register tanımları
+#include <stdint.h>
+// STM32G071RB için gerekli clock GPIO pin register tanımlamaları
 #define RCC_BASE        0x40021000
 #define RCC_IOPENR      (*(volatile unsigned int *)(RCC_BASE + 0x34))
-#define RCC_AHBENR		(*(volatile unsigned int *)(RCC_BASE + 0x38))
+#define RCC_AHBENR      (*(volatile unsigned int *)(RCC_BASE + 0x38))
+
 #define GPIOA_BASE      0x50000000
 #define GPIOA_MODER     (*(volatile unsigned int *)(GPIOA_BASE + 0x00))
 #define GPIOA_ODR       (*(volatile unsigned int *)(GPIOA_BASE + 0x14))
+
 #define GPIOAEN         (1U << 0)
 #define PIN0            (1U << 0)
 #define LED_PIN         PIN0
 
+#define DELAY_COUNT     8000000  // 500ms gecikme için yaklaşık değer
 
+void delay(volatile uint32_t count) {
+    while (count--) {
+        // Boş döngü ile gecikme
+    }
+}
 
 int main(void) {
-    // System Clock ayarları
 
-	RCC_AHBENR |= 0x1;
+    RCC_IOPENR |= GPIOAEN;  // GPIOA'nın clock'u etkinleştiriliyor
 
-    RCC_IOPENR |= GPIOAEN;  // RCC_IOPENR_IOPAEN
 
-    // PA0 pinini çıkış olarak ayarlama
-    GPIOA_MODER &= (1U << (2 * 0));
-
+    GPIOA_MODER &= ~(3U << (2 * 0));
+    GPIOA_MODER |= (1U << (2 * 0));   // PA0'u çıkış modu olarak ayarladık
 
     while (1) {
         // LED'i yak
         GPIOA_ODR |= LED_PIN;
-        delay_ms(500);
+        delay(DELAY_COUNT);
 
         // LED'i söndür
         GPIOA_ODR &= ~LED_PIN;
-        delay_ms(500);
+        delay(DELAY_COUNT);
     }
 }
-
-
